28bc2d47b5b5fbacb5f525808c70c0c7033e34f2
mikev-pyflame <50948598+mikev-pyflame@users.noreply.github.com>
2021-04-29 00:35:55 -0700

    Add files via upload

---
+++ flame_label.py
@@ -0,0 +1,32 @@
+class FlameLabel(QtWidgets.QLabel):
+    """
+    Custom Qt Flame Label Widget
+
+    For different label looks use: label_type='normal', label_type='background', or label_type='outline'
+
+    To use:
+
+    label = FlameLabel('Label Name', label_type='normal', window)
+    """
+
+    def __init__(self, label_name, label_type, parent_window, *args, **kwargs):
+        super(FlameLabel, self).__init__(*args, **kwargs)
+
+        self.setText(label_name)
+        self.setParent(parent_window)
+        self.setMinimumSize(110, 28)
+        self.setMaximumHeight(28)
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+
+        # Set label stylesheet based on label_type
+
+        if label_type == 'normal':
+            self.setStyleSheet('QLabel {color: #9a9a9a; border-bottom: 1px inset #282828; font: 14px "Discreet"}'
+                               'QLabel:disabled {color: #6a6a6a}')
+        elif label_type == 'background':
+            self.setAlignment(QtCore.Qt.AlignCenter)
+            self.setStyleSheet('color: #9a9a9a; background-color: #393939; font: 14px "Discreet"')
+        elif label_type == 'outline':
+            self.setAlignment(QtCore.Qt.AlignCenter)
+            self.setStyleSheet('color: #9a9a9a; background-color: #212121; border: 1px solid #404040; font: 14px "Discreet"')
+
