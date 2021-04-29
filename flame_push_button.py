b63923937580f8a4da696e84006f6691491f96bd
mikev-pyflame <50948598+mikev-pyflame@users.noreply.github.com>
2021-04-28 23:54:46 -0700

    Add files via upload

---
+++ flame_push_button.py
@@ -0,0 +1,23 @@
+class FlamePushButton(QtWidgets.QPushButton):
+    """
+    Custom Qt Flame Push Button Widget
+
+    To use:
+
+    pushbutton = FlamePushButton(' Button Name', true_or_false, window)
+    """
+
+    def __init__(self, button_name, button_is_checked, parent, *args, **kwargs):
+        super(FlamePushButton, self).__init__(*args, **kwargs)
+
+        self.setText(button_name)
+        self.setParent(parent)
+        self.setCheckable(True)
+        self.setChecked(checked)
+        self.setMinimumSize(110, 28)
+        self.setMaximumSize(110, 28)
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.setStyleSheet('QPushButton {color: #9a9a9a; background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: .90 #424142, stop: .91 #2e3b48); text-align: left; border-top: 1px inset #555555; border-bottom: 1px inset black; font: 14px "Discreet"}'
+                           'QPushButton:checked {color: #d9d9d9; background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: .90 #4f4f4f, stop: .91 #5a7fb4); font: italic; border: 1px inset black; border-bottom: 1px inset #404040; border-right: 1px inset #404040}'
+                           'QPushButton:disabled {color: #6a6a6a; background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: .90 #383838, stop: .91 #353535); font: light; border-top: 1px solid #575757; border-bottom: 1px solid #242424; border-right: 1px solid #353535; border-left: 1px solid #353535}'
+                           'QToolTip {color: black; background-color: #ffffde; border: black solid 1px}')
