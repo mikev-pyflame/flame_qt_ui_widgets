09a350854274657a49827c0f894d340f9e9f5f2a
mikev-pyflame <50948598+mikev-pyflame@users.noreply.github.com>
2021-04-28 23:49:15 -0700

    Add files via upload

---
+++ flame_line_edit.py
@@ -0,0 +1,22 @@
+class FlameLineEdit(QtWidgets.QLineEdit):
+    """
+    Custom Qt Flame Line Edit Widget
+
+    Main window should include this: window.setFocusPolicy(QtCore.Qt.StrongFocus)
+
+    To use:
+
+    line_edit = FlameLineEdit('Some text here', window)
+    """
+
+    def __init__(self, text, parent, *args, **kwargs):
+        super(FlameLineEdit, self).__init__(*args, **kwargs)
+
+        self.setText(text)
+        self.setParent(parent)
+        self.setMinimumHeight(28)
+        self.setMinimumWidth(110)
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.setStyleSheet('QLineEdit {color: #9a9a9a; background-color: #373e47; selection-color: #262626; selection-background-color: #b8b1a7; font: 14px "Discreet"}'
+                           'QLineEdit:focus {background-color: #474e58}'
+                           'QLineEdit:disabled {color: #6a6a6a; background-color: #373737}')
