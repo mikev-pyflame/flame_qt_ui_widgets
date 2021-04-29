66a8b9131cc13c66b48ff4e2d408b26169ce318f
mikev-pyflame <50948598+mikev-pyflame@users.noreply.github.com>
2021-04-29 00:48:40 -0700

    Add files via upload

---
+++ flame_button.py
@@ -0,0 +1,21 @@
+class FlameButton(QtWidgets.QPushButton):
+    """
+    Custom Qt Flame Button Widget
+
+    To use:
+
+    button = FlameButton('Button Name', do_this_when_pressed, window)
+    """
+
+    def __init__(self, button_name, do_when_pressed, parent_window, *args, **kwargs):
+        super(FlameButton, self).__init__(*args, **kwargs)
+
+        self.setText(button_name)
+        self.setParent(parent_window)
+        self.setMinimumSize(QtCore.QSize(110, 28))
+        self.setMaximumSize(QtCore.QSize(110, 28))
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.clicked.connect(do_when_pressed)
+        self.setStyleSheet('QPushButton {color: #9a9a9a; background-color: #424142; border-top: 1px inset #555555; border-bottom: 1px inset black; font: 14px "Discreet"}'
+                           'QPushButton:pressed {color: #d9d9d9; background-color: #4f4f4f; border-top: 1px inset #666666; font: italic}'
+                           'QPushButton:disabled {color: #747474; background-color: #353535; border-top: 1px solid #444444; border-bottom: 1px solid #242424}')
